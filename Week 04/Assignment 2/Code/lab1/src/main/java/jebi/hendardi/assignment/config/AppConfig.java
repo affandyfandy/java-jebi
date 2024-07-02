29a9c226f8f3072394e0799cb8fab43507269dfa
ryuzakijebi <ryuzaki.jebi@gmail.com>
2024-07-02 12:37:58 +0700

    upload

--- Week 04/Assignment 2/Code/lab1/src/main/java/jebi/hendardi/assignment/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package jebi.hendardi.assignment.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "jebi.hendardi.assignment")
-public class AppConfig {
-}
